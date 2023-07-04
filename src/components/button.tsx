ffbf20656b5d47b7c0249f54060f194e3f567905
Corbin Crutchley <git@crutchcorn.dev>
2023-07-04 13:07:07 -0700

    buttons colors done now

---
+++ src/components/button.tsx
@@ -0,0 +1,80 @@
+import {
+  AnchorHTMLAttributes,
+  ButtonHTMLAttributes,
+  createElement,
+  PropsWithChildren,
+  ReactNode,
+} from "react";
+
+import styles from "./button.module.scss";
+
+type ButtonProps = PropsWithChildren<
+  {
+    tag?: "a" | "button";
+    class?: string;
+    variant?: "primary" | "secondary";
+  } & ButtonHTMLAttributes<HTMLButtonElement> &
+    AnchorHTMLAttributes<HTMLAnchorElement>
+>;
+
+function ButtonWrapper({
+  tag = "a",
+  class: className,
+  children,
+  variant = "primary",
+  ...props
+}: ButtonProps) {
+  const Wrapper = (props: any) => createElement(tag, props, props.children);
+
+  return (
+    <Wrapper
+      {...props}
+      class={[styles.buttonBase, className, variant]
+        .filter((c) => !!c)
+        .join(" ")}
+    >
+      {children}
+    </Wrapper>
+  );
+}
+
+export function Button({ class: className = "", ...props }: ButtonProps) {
+  return (
+    <ButtonWrapper {...props} class={`${styles.nonIconButton} ${className}`} />
+  );
+}
+
+interface IconAndTextButtonProps extends ButtonProps {
+  leftIcon?: ReactNode;
+}
+
+export function IconAndTextButton({
+  class: className = "",
+  leftIcon,
+  children,
+  ...props
+}: IconAndTextButtonProps) {
+  return (
+    <ButtonWrapper {...props} class={`${styles.iconButton} ${className}`}>
+      <span className={styles.iconButtonButton}>{leftIcon}</span>
+      {children}
+    </ButtonWrapper>
+  );
+}
+
+interface IconOnlyButtonProps extends ButtonProps {
+  icon?: ReactNode;
+}
+
+export function IconOnlyButton({
+  class: className = "",
+  icon,
+  children,
+  ...props
+}: IconOnlyButtonProps) {
+  return (
+    <ButtonWrapper {...props} class={`${styles.iconOnlyButton} ${className}`}>
+      <span>{icon}</span>
+    </ButtonWrapper>
+  );
+}
