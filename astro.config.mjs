1434fe5581d2b7aa3601798327cbd0f4d45a1fce
Joseph Murillo <josephmurillo2425@gmail.com>
2026-02-18 15:21:39 -0600

    setup new page rip wallet

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
+// @ts-check
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://ripwallet.co",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/components/seo/shared.ts
+++
@@ -1,9 +0,0 @@
-export interface SEOProps {
-	description?: string;
-	title: string;
-	keywords?: string[];
-	pathName?: string;
-	canonical?: string;
-	isbn?: string;
-	shareImage?: string;
-}
